7749e37313a9efc484e3f6c6c760f86fb34d8a36
mlab817 <mlab817@gmail.com>
2022-09-22 16:54:51 +0800

    removed algolia

--- src/theme/ColorModeToggle/index.js
+++ src/theme/ColorModeToggle/index.js
@@ -5,9 +5,9 @@ import {logout} from '../firebase';
 export default function ColorModeToggleWrapper(props) {
   return (
     <>
-      <a style={{marginRight: 15, cursor: "pointer", color: '#222222'}} onClick={() => logout(() => window.location.reload())}>
+      {/* <a style={{marginRight: 15, cursor: "pointer", color: '#222222'}} onClick={() => logout(() => window.location.reload())}>
         Logout
-      </a>
+      </a> */}
       <ColorModeToggle {...props} />
     </>
   );
